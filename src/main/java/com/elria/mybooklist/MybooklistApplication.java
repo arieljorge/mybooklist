ac5d200112f6c2118cb9cdfb9f11ce0d3d9bc0d2
ariel_jorge <accarieljorge@gmail.com>
2025-01-21 21:06:51 -0300

    Project create

---
+++ src/main/java/com/elria/mybooklist/MybooklistApplication.java
@@ -0,0 +1,13 @@
+package com.elria.mybooklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybooklistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybooklistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elria/mybooklist/MybooklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elria.mybooklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybooklistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
